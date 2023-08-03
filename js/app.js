3f0a428483618c4536d4fbe19213ec2c301ffb33
Vinícius Moura Spindola <viniciusmspindola@gmail.com>
2023-08-03 14:47:31 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -47,6 +47,61 @@ $('.cases').slick({
         }
     ]
 });
+$('.plataformas').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '',
+    nextArrow: '',
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.implementacoes').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '.prev-implementacoes',
+    nextArrow: '.next-implementacoes',
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+        }
+    ]
+});
 
 $(window).on('scroll',()=>{
     if($(window).scrollTop() > 30){
---
+++ js/banners.js
@@ -0,0 +1,8 @@
+$('#banners-carrossel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: '',
+    nextArrow: '',
+});
